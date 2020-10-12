e4b519e2167ca01bd07c87eb54f6cdba403b23f4
Rishabh gajra <rgrishigajra@gmail.com>
2020-10-12 13:06:45 -0400

    feat:full Reducer and mapper minor changes Added reducer code, updated mapper to get id from key value store

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -16,6 +16,8 @@ class mapper:
     LOG = log_helper._logger("map-reduce mapper")
 
     def send_mapper_output(self, mapper_output):
+        self.LOG.log(50, 'Sending mapper out put for ' +
+                     str(self.mapper_id)+" to key value store")
         for list_item in mapper_output:
             # hashing to assign to a reducer
             reducer_id = int(fnv1a_32(list_item[0])) % int(
@@ -23,27 +25,31 @@ class mapper:
             msg_key = "reducer"+str(reducer_id)+"mapper"+str(self.mapper_id)
             msg_val = str(list_item[0]+" "+str(list_item[1])+"\n")
             self.mapper_client.append_key(msg_key, msg_val)
+        self.LOG.log(50, 'output for mapper ' +
+                     str(self.mapper_id) + ' stored in key value store')
         return True
 
     def run_serialized_mapper(self, key, value):
         self.LOG.log(
-            20, 'Running supplied mapper on worker '+str(self.mapper_id))
+            50, 'Running supplied mapper on worker '+str(self.mapper_id))
         with open(self.config['app_config']['MapperCodeSerialized'], 'rb') as fd:
             code_string = fd.read()
             code = marshal.loads(code_string)
             user_defined_map_function = types.FunctionType(
-                code, globals(), "user_defined_map_function")
+                code, globals(), "user_defined_map_function"+str(self.mapper_id))
             mapper_output = user_defined_map_function(key, value)
+            self.LOG.log(50, 'Mapper '+str(self.mapper_id) +
+                         " has run mapper function")
         return mapper_output
 
     def get_mapper_data(self):
-        self.mapper_id = self.mapper_client.get_assignment(
-            self.mapper_name, self.mapper_port)
-        keys = self.mapper_client.get_mapper_keys(self.mapper_id)
+        # self.mapper_id = self.mapper_client.get_assignment(
+        #     self.mapper_name, self.mapper_port)
+        keys = self.mapper_client.get_keys("mapper"+str(self.mapper_id))
         data = []
         for key in keys:
             data = self.mapper_client.get_key_lines(key)
-            self.LOG.log(20, "mapper "+str(self.mapper_id) +
+            self.LOG.log(50, "mapper "+str(self.mapper_id) +
                          " received data "+str(len(data)))
             kay_val_split_msg = data.split(' \r\n')
             key_value_pair = [data.split(' \r\n')[0].split(
@@ -54,18 +60,31 @@ class mapper:
                 key_value_pair[0], key_value_pair[1])
             # gets a list of (key,value) pairs, storing it in kay value store for each reducer
             self.send_mapper_output(mapper_output)
-            break
+        self.LOG.log(50, 'mapper '+str(self.mapper_id)+" done")
         return True
+
+    def get_mapper_id(self):
+        keys = self.mapper_client.get_keys('mapper_status')
+        for key in keys:
+            val = self.mapper_client.get_key(key)
+            if val.split(' \r\n')[1] == 'idle':
+                id = val.split(' \r\n')[0].split()[1][len('mapper_status'):]
+                self.mapper_client.set_key('mapper_status'+str(id), 'assigned')
+                return int(id)
+        return 99999
 
     def __init__(self, mapper_name, mapper_port):
         self.mapper_port = mapper_port
         self.mapper_name = mapper_name
-        self.LOG.log(50, "Booting up map-reduce mapper")
         self.mapper_client = client(
             int(self.config['app_config']['KeyValueServerPort']))
+        self.mapper_id = self.get_mapper_id()
+        self.LOG.log(50, "Booting up map-reduce mapper with id " +
+                     str(self.mapper_id))
+
         # if not self.mapper_client.ping_server():
         #     self.LOG.log(50, 'key-value is store offline')
         #     exit()
-        self.get_mapper_data()
+        self.LOG.log(50, 'mapper '+str(self.mapper_id)+" intialized")
         # self.map()
         return
---
+++ reducer/reducer.py
@@ -0,0 +1,87 @@
+from key_value_pair_cache.client import client
+import key_value_pair_cache.helper_functions.log_helper as log_helper
+import configparser
+import os
+import sys
+from collections import defaultdict
+import marshal
+import types
+import re
+from key_value_pair_cache.helper_functions.hash_fnv1a import fnv1a_32
+
+
+class reducer:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    LOG = log_helper._logger("map-reduce reducer")
+
+    def send_reducer_output(self, reducer_output):
+        self.LOG.log(50, 'Sending reducer out put for ' +
+                     str(self.reducer_id)+" to key value store")
+        msg_key = 'reducer_output' + str(self.reducer_id)
+        for list_item in reducer_output:
+            msg_val = str(list_item[0]+" "+str(list_item[1])+"\n")
+            self.reducer_client.append_key(msg_key, msg_val)
+        self.LOG.log(50, 'output for reducer ' +
+                     str(self.reducer_id) + 'stored in key value store')
+        return True
+
+    def run_serialized_reducer(self, input_list):
+        self.LOG.log(
+            50, 'Running supplied reducer on worker '+str(self.reducer_id))
+        with open(self.config['app_config']['ReducerCodeSerialized'], 'rb') as fd:
+            code_string = fd.read()
+            code = marshal.loads(code_string)
+            user_defined_reduce_function = types.FunctionType(
+                code, globals(), "user_defined_reducer_function"+str(self.reducer_id))
+        reducer_output = user_defined_reduce_function(input_list)
+        self.LOG.log(50, 'Reducer '+str(self.reducer_id) +
+                     " has run reducer function")
+        return reducer_output
+
+    def get_reducer_data(self):
+        keys = self.reducer_client.get_keys("reducer"+str(self.reducer_id))
+        data = ''
+        for key in keys:
+            data = self.reducer_client.get_key_lines(key)
+            self.LOG.log(50, "reducer "+str(self.reducer_id) +
+                         " received data "+str(len(data)))
+            lines = data.split('\n')[1:]
+            input_list = []
+            # split from lines to list of tuples
+            for line in lines:
+                words = line.split()
+                if len(words) == 2:
+                    input_list.append((words[0], words[1]))
+        self.LOG.log(50, "reducer "+str(self.reducer_id) +
+                     " collected these keys data :"+str(len(input_list)))
+        # runs serial reducer script
+        reducer_output = self.run_serialized_reducer(input_list)
+        self.send_reducer_output(reducer_output)
+        return True
+
+    def get_reducer_id(self):
+        keys = self.reducer_client.get_keys('reducer_status')
+        for key in keys:
+            val = self.reducer_client.get_key(key)
+            if val.split(' \r\n')[1] == 'idle':
+                id = val.split(' \r\n')[0].split()[1][len('reducer_status'):]
+                self.reducer_client.set_key('reducer_status'+str(id), 'assigned')
+                return int(id)
+        return 99999
+
+    def __init__(self, reducer_name, reducer_port):
+        self.reducer_port = reducer_port
+        self.reducer_name = reducer_name
+        self.reducer_client = client(
+            int(self.config['app_config']['KeyValueServerPort']))
+        self.reducer_id = self.get_reducer_id()
+        self.LOG.log(50, "Booting up map-reduce reducer with id " +
+                     str(self.reducer_id))
+
+        # if not self.reducer_client.ping_server():
+        #     self.LOG.log(50, 'key-value is store offline')
+        #     exit()
+        self.LOG.log(50, 'reducer '+str(self.reducer_id)+" intialized")
+        # self.map()
+        return
