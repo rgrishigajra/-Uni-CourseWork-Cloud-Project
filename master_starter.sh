11e7e856f0026786f670d8d4b1dc74b940fc2c68
Rishabh gajra <rgrishigajra@gmail.com>
2020-10-24 03:14:54 -0400

    pushing logs

--- master_starter.sh
+++ master_starter.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 git clone https://github.com/rgrishigajra/Uni-CourseWork-Cloud-Project.git
 cd Uni-CourseWork-Cloud-Project
+sudo apt-get update
+sudo apt-get -y install python3-pip
+pip3 install flask
 # you can comment the line below to read logs
 sudo python3 master_init.py
-# sudo python3 flaskserver.py
