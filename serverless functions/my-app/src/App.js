34081b8b6c5d92caf4ee98e9931cede86e150f39
Rishabh gajra <rgrishigajra@gmail.com>
2020-11-30 15:32:43 -0500

    chore:starter code front end Added file structure and starter code for a react app.

---
+++ serverless functions/my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './containers/Homepage/Homepage';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ serverless functions/my-app/src/containers/Homepage/Homepage.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import './Homepage.css';
+function Homepage() {
+  return <div className="body-container">Heyo</div>;
+}
+
+export default Homepage;
