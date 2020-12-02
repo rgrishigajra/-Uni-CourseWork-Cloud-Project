ba888999b4772c5219ed73faeb17e41603b776ff
Rishabh gajra <rgrishigajra@gmail.com>
2020-12-02 03:52:15 -0500

    feat:functions for caching added saving to firestore on main api and a support api to check and return past results.

--- serverless functions/sentence_length/main.py
+++ serverless functions/sentence_length/main.py
@@ -2,6 +2,8 @@ from flask import escape, jsonify
 from collections import defaultdict
 import requests
 import re
+from google.cloud import firestore
+import json
 
 
 def sentence_length(request):
@@ -54,17 +56,52 @@ def sentence_length(request):
     text = resp.text
     sentences = split_into_sentences(text)
     length_frequency = defaultdict(int)
-    for i in range(0,60):
+    for i in range(0, 60):
         length_frequency[i] = 0
     for sentence in sentences:
         words = re.findall(r'\w+', sentence)
         l = len(words)
         if l < 60:
             length_frequency[l] += 1
-    
+
     response = jsonify({
         'frequency': length_frequency
     })
+    response.headers.add('Access-Control-Allow-Headers',
+                         "Content-Type,Authorization,true")
+    response.headers.add('Access-Control-Allow-Methods',
+                         'GET,PUT,POST,PATCH,DELETE,OPTIONS')
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    db = firestore.Client()
+    book_id = url.split('/')[-2]
+    doc_ref = db.collection(u'books').document(book_id)
+    doc_ref.set({
+        u'frequency': json.dumps(length_frequency)
+    })
+    return response
+
+
+def cached_data(request):
+    request_json = request.get_json(silent=True)
+    if request_json and 'url' in request_json:
+        url = request_json['url']
+    else:
+        url = 'http://www.gutenberg.org/files/1342/1342-0.txt'
+    book_id = url.split('/')[-2]
+    db = firestore.Client()
+    doc_ref = db.collection(u'books').document(book_id)
+    doc = doc_ref.get()
+    if doc.exists:
+        freq = json.loads(doc.to_dict()['frequency'])
+        response = jsonify({
+            'success': True,
+            'frequency': freq,
+        })
+    else:
+        response = jsonify({
+            'success': False
+        })
+
     response.headers.add('Access-Control-Allow-Headers',
                          "Content-Type,Authorization,true")
     response.headers.add('Access-Control-Allow-Methods',
